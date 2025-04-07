059ed4a4d9004585c50f5259441da337dfc4b9da
emanuelrestrepo22 <emadavresgar@gmail.com>
2025-04-07 18:58:40 -0300

    added TC02 successfully

--- cypress/e2e/crud_api.cy.js
+++ cypress/e2e/crud_api.cy.js
@@ -1,10 +1,10 @@
 import * as helpers from '../support/helpers';
 import user from '../fixtures/user.json';
 
-describe('🧪 API Testing - Modularizado con helpers', () => {
+describe('🧪 TS01 - API Testing - Modularizado con helpers', () => {
   let userId;
 
-  it('🔹 Crear un nuevo usuario', () => {
+  it(' TS01-TC01. Crear un nuevo usuario', () => {
     helpers.createUser(user.initial).then((res) => {
       expect(res.status).to.eq(201);
       expect(res.body).to.have.property('_id');
@@ -13,5 +13,16 @@ describe('🧪 API Testing - Modularizado con helpers', () => {
     });
   });
 
-  // Podrás agregar más tests aquí usando el mismo userId y helpers.
+  it('TS01-TC02. Validar que el usuario fue creado', () => {
+    helpers.createUser(user.initial).then((res) => {
+      userId = res.body._id;
+      cy.log('Usuario creado con ID:', userId);
+
+      helpers.getUser(userId).then((res) => {
+        expect(res.status).to.eq(200);
+        expect(res.body).to.have.property('name', user.initial.name);
+        expect(res.body).to.have.property('job', user.initial.job);
+      });
+    });
+  });
 });
